1e783575ec9e00e14c44e11ea3a946580f14be97
Abhishek Kedia <iamabhikedia74@gmail.com>
2019-09-12 17:48:08 +0530

    Mainpage added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react'
-
+import Mainpage from './mainpage'
 export class App extends PureComponent {
   render() {
     return (
       <div>
-        
+        <Mainpage />
       </div>
     )
   }
--- src/Components/abi2.js
+++ src/Components/abi2.js
@@ -4,10 +4,52 @@ import Web3 from 'web3';
 const portis = new Portis('467c1a86-08d3-49bd-b8d8-7211127ca579', 'ropsten');
 const web3 = new Web3(portis.provider);
 
-var abi=[
+var abi =[
 	{
 		"constant": true,
-		"inputs": [],
+		"inputs": [
+			{
+				"internalType": "bytes",
+				"name": "sn",
+				"type": "bytes"
+			}
+		],
+		"name": "get_value2",
+		"outputs": [
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "bytes",
+				"name": "sn",
+				"type": "bytes"
+			}
+		],
 		"name": "check",
 		"outputs": [
 			{
@@ -21,36 +63,98 @@ var abi=[
 		"type": "function"
 	},
 	{
-		"constant": false,
+		"constant": true,
 		"inputs": [
 			{
-				"internalType": "address",
-				"name": "_add",
-				"type": "address"
-			},
-			{
 				"internalType": "bytes",
-				"name": "name",
-				"type": "bytes"
-			},
-			{
-				"internalType": "bytes",
-				"name": "reg",
-				"type": "bytes"
-			},
-			{
-				"internalType": "bytes",
-				"name": "loc",
+				"name": "sn",
 				"type": "bytes"
 			}
 		],
-		"name": "new_registration",
+		"name": "get_value1",
+		"outputs": [
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "bytes",
+				"name": "sn",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "cid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "mid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "mmid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "hid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "sid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "gid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "bid",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "stag",
+				"type": "bytes"
+			}
+		],
+		"name": "new_model",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	}
 ];
-var token2 = new web3.eth.Contract(abi,'0xdadc346cd0d1273a907639a23827de5f4b604cdd');
 
-export default token2;
+
+var token = new web3.eth.Contract(abi, '0x50c1e9cf35a7ecee40057dd1c61c8f5e1ddeb175');
+export default token;
